c701d46fef710e6f25fe662787b18a435d1454f3
TTumyr <trond.tumyr@gmail.com>
2021-09-15 14:33:14 +0200

    fixed add and back buttons

--- app/src/main/java/com/example/todolist/TaskSettings.kt
+++ app/src/main/java/com/example/todolist/TaskSettings.kt
@@ -5,20 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 import com.example.todolist.databinding.ActivityMainBinding
+import com.example.todolist.databinding.ActivityTaskSettingsBinding
 
 class TaskSettings : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityTaskSettingsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityTaskSettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
-        //val bBack = binding.ivBack as ImageView
-        //bBack.setOnClickListener {
-        //    val intent = Intent(this, TaskSettings::class.java)
-        //    startActivity(intent)
-        //}
+        val bBack = binding.ivBack as ImageView
+        bBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
